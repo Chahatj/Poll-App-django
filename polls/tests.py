9eaad10d6e68041470cfb339fa9af8c8421123df
chahat <chahat.jain0@gmail.com>
2018-06-18 19:51:34 +0530

    writing test for detail view

--- polls/tests.py
+++ polls/tests.py
@@ -60,3 +60,15 @@ class QuestionIndexViewTests(TestCase):
         self.assertQuerysetEqual(response.context['latest_question_list'],
                                  ['<Question: 1 past question>', '<Question: 2 past question>']
                                  )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text="Future question", days=5)
+        response = self.client.get(reverse('polls:detail', args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text="Past question", days=-5)
+        response = self.client.get(reverse('polls:detail', args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text)
